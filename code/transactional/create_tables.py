2dd5532e5cf525bb9d84470470710e2bc74a268e
andre-ls <andre.luis.e3@gmail.com>
2024-11-27 20:30:51 -0300

    Adição de Main no Código Transacional

--- code/transactional/create_tables.py
+++ code/transactional/create_tables.py
@@ -1,11 +1,15 @@
 import psycopg2
 from queries import *
 
-conn = psycopg2.connect("host=some-postgres dbname=postgres user=postgres password=postgres")
-cur = conn.cursor()
+def createTables(conn, cursor)
+    queries = [olist_customers, olist_geolocation, olist_order_items, olist_order_payments, olist_order_reviews, olist_orders, olist_products, olist_sellers, olist_product_category_name_translation]
 
-queries = [olist_customers, olist_geolocation, olist_order_items, olist_order_payments, olist_order_reviews, olist_orders, olist_products, olist_sellers, olist_product_category_name_translation]
+    print(">> Criação das Tabelas Transacionais")
+    for query in queries:
+        cursor.execute(query)
+        conn.commit()
 
-for query in queries:
-    cur.execute(query)
-    conn.commit()
+if __name__ == "__main__":
+    conn = psycopg2.connect("host=some-postgres dbname=postgres user=postgres password=postgres")
+    cur = conn.cursor()
+    createTable(conn, cur)
--- code/transactional/insert_data.py
+++ code/transactional/insert_data.py
@@ -5,7 +5,7 @@ def generateInsertQuery(table_name,header):
     values_position = ','.join(['%s']*len(header))
     return f"INSERT INTO {table_name} VALUES ({values_position})"
 
-def insertData(table_name, file_path):
+def insertFiles(table_name, file_path):
     with open(file_path, 'r') as f:
         reader = csv.reader(f)
         header = next(reader) # Skip the header row.
@@ -15,20 +15,26 @@ def insertData(table_name, file_path):
             cur.execute(query,clean_row)
     conn.commit()
 
-files = [
-    {'name' : 'olist_customers' , 'path' : 'data/olist_customers_dataset.csv'},
-    {'name' : 'olist_geolocation' , 'path' : 'data/olist_geolocation_dataset.csv'},
-    {'name' : 'olist_order_items' , 'path' : 'data/olist_order_items_dataset.csv'},
-    {'name' : 'olist_order_payments' , 'path' : 'data/olist_order_payments_dataset.csv'},
-    {'name' : 'olist_order_reviews' , 'path' : 'data/olist_order_reviews_dataset.csv'},
-    {'name' : 'olist_orders' , 'path' : 'data/olist_orders_dataset.csv'},
-    {'name' : 'olist_products' , 'path' : 'data/olist_products_dataset.csv'},
-    {'name' : 'olist_sellers' , 'path' : 'data/olist_sellers_dataset.csv'},
-    {'name' : 'olist_product_category_name_translation' , 'path' : 'data/product_category_name_translation.csv'}
-]
+def insertData(conn, cursor):
+    files = [
+        {'name' : 'olist_customers' , 'path' : 'data/olist_customers_dataset.csv'},
+        {'name' : 'olist_geolocation' , 'path' : 'data/olist_geolocation_dataset.csv'},
+        {'name' : 'olist_order_items' , 'path' : 'data/olist_order_items_dataset.csv'},
+        {'name' : 'olist_order_payments' , 'path' : 'data/olist_order_payments_dataset.csv'},
+        {'name' : 'olist_order_reviews' , 'path' : 'data/olist_order_reviews_dataset.csv'},
+        {'name' : 'olist_orders' , 'path' : 'data/olist_orders_dataset.csv'},
+        {'name' : 'olist_products' , 'path' : 'data/olist_products_dataset.csv'},
+        {'name' : 'olist_sellers' , 'path' : 'data/olist_sellers_dataset.csv'},
+        {'name' : 'olist_product_category_name_translation' , 'path' : 'data/product_category_name_translation.csv'}
+    ]
 
-conn = psycopg2.connect("host=some-postgres dbname=postgres user=postgres password=postgres")
-cur = conn.cursor()
-for file in files:
-    print(f">>> Upload da Tabela {file['name']}")
-    insertData(file['name'],file['path'])
+    print(">> Upload dos Dados Transacionais")
+    for file in files:
+        print(f"    >>> Upload da Tabela {file['name']}")
+        insertFiles(file['name'],file['path'])
+
+if __name__ == "__main__":
+    conn = psycopg2.connect("host=some-postgres dbname=postgres user=postgres password=postgres")
+    cur = conn.cursor()
+
+    insertData(conn, cur)
