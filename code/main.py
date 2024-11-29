6edc9d2ba9f26648f3720380c8b35f69a30e7766
andre-ls <andre.luis.e3@gmail.com>
2024-11-29 00:04:16 -0300

    Adição de compose.yml

--- code/main.py
+++ code/main.py
@@ -1,3 +1,4 @@
+import os
 import psycopg2
 import transactional.create_tables as transactional_create
 import transactional.insert_data as transactional_insert
@@ -6,11 +7,15 @@ import star_schema.insert_data as starschema_insert
 import wide_table.create_tables as widetable_create
 import wide_table.insert_data as widetable_insert
 
+transactional_db = os.getenv('TRANSACTIONAL_DB')
+starschema_db = os.getenv('STARSCHEMA_DB')
+widetable_db = os.getenv('WIDETABLE_DB')
+
 # Conexões com Banco de Dados
 
-transactional_connection = psycopg2.connect("host=some-postgres dbname=postgres user=postgres password=postgres")
-starschema_connection = psycopg2.connect("host=star-postgres dbname=postgres user=postgres password=postgres")
-widetable_connection = psycopg2.connect("host=wide-postgres dbname=postgres user=postgres password=postgres")
+transactional_connection = psycopg2.connect(f"host={transactional_db} dbname=postgres user=postgres password=postgres")
+starschema_connection = psycopg2.connect(f"host={starschema_db} dbname=postgres user=postgres password=postgres")
+widetable_connection = psycopg2.connect(f"host={widetable_db} dbname=postgres user=postgres password=postgres")
 
 # Setup do Banco de Dados Transacional
 transactional_create.createTables(transactional_connection)
